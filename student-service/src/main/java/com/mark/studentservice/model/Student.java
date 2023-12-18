545438f9dc7f288924873c8b094860e33a3bb5ff
markaboian <abomark09@gmail.com>
2023-12-18 18:39:45 -0300

    added ms-student and eureka-server

---
+++ student-service/src/main/java/com/mark/studentservice/model/Student.java
@@ -0,0 +1,17 @@
+package com.mark.studentservice.model;
+
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+
+//@Entity
+public class Student {
+//    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String gender;
+    private String address;
+
+
+}
